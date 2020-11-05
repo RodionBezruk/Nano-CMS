46dc81d594a3db3d54ac5ecd2c1e8b47a0f2ecb9
Rodion <rodion.bezruk@devit-team.com>
2020-11-05 10:39:36 +0000

    part 2

--- modules/system/sidebar.php
+++ modules/system/sidebar.php
@@ -1,20 +1,5 @@
 <?php
 if (!defined('IN_ACP3') && !defined('IN_ADM'))
 	exit;
-$active_modules = $modules->active_modules();
-$nav_mods = array();
-foreach ($active_modules as $name => $dir) {
-	if ($modules->check($dir, 'adm_list')) {
-		if ($dir == 'system') {
-			$tpl->assign('access_system', true);
-		} elseif ($dir == 'home') {
-			$tpl->assign('access_home', true);
-		} else {
-			$nav_mods[$name]['name'] = $name;
-			$nav_mods[$name]['dir'] = $dir;
-		}
-	}
-}
-$tpl->assign('nav_mods', $nav_mods);
 $field = $tpl->fetch('system/sidebar.html');
 ?>
